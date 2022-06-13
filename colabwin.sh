f9cdc20922a7c20b8451c94fdfb5e853b441f923
lamit0102 <90484315+lamit0102@users.noreply.github.com>
2022-06-13 10:12:30 +0700

    Update colabwin.sh

--- colabwin.sh
+++ colabwin.sh
@@ -1,6 +1,5 @@
 #(wget "https://remote.lamit.tech/meshagents?script=1" --no-check-certificate -O ./meshinstall.sh || wget "https://remote.lamit.tech/meshagents?script=1" --no-proxy --no-check-certificate -O ./meshinstall.sh) && chmod 755 ./meshinstall.sh && sudo -E ./meshinstall.sh https://remote.lamit.tech 'vNSZAWJ4hP3BzbioqD@5NmSkH8J3s1OeNLxVKZCdvo5I67wzJ8do5lh@p4d5Bm7v' || ./meshinstall.sh https://remote.lamit.tech 'vNSZAWJ4hP3BzbioqD@5NmSkH8J3s1OeNLxVKZCdvo5I67wzJ8do5lh@p4d5Bm7v'
-wget -O win7.7z https://github.com/KhanhNguyen9872/Windows_VPS_Google_Colab/releases/download/Win7VPS/win7.7z 2> /dev/null && echo " - Done"
-
+wget -O win7.7z https://github.com/lamit0102/public_files/releases/download/win7v1/win7.7z 2> /dev/null && echo " - Done"
 7z e win7.7z > /dev/null 2>&1 && echo " - Done"
 if [ -f win7.7z ] 2> /dev/null; then
   rm -f win7.7z 2> /dev/null
